a7673a15a7abeafc6512dfc6b8ec257d617f9512
Mr091522 <mrandall7415@sdsu.edu>
2026-09-22 22:37:07 -0700

    Fixed import Form from './Form.jsx'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
-import Form from "Form.jsx"
+import Form from './Form.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
