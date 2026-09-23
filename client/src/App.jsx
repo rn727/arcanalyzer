7e4380196c3b857f77d51455f5ab534e39a6885e
Mr091522 <mrandall7415@sdsu.edu>
2026-09-22 23:10:15 -0700

    Fixed lint errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+
 import './App.css'
 import Form from './Form.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Form />
