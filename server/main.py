7c45e2ab3a48a54a48e192631f73f31fd3e8ea63
EricSDSU <emiao0532@sdsu.edu>
2026-09-22 23:24:25 -0700

    Make CORS permissive

--- server/main.py
+++ server/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Review(BaseModel):
     rating: int # 0-10
